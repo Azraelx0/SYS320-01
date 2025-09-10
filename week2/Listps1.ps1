2dea2e9f04a2a971e2d4e79047a615504eb0b7b9
cade <caden.mercer@mymail.champlain.edu>
2025-09-09 22:00:49 -0400

    Added Listps1.psq scripts and outfolder

---
+++ week2/Listps1.ps1
@@ -0,0 +1,34 @@
+﻿cd $PSScriptRoot
+
+$files=(Get-ChildItem)
+for ($j=0; $j -le $files.length; $j++){
+    
+    if ($files[$j].name -ilike "*ps1"){
+        Write-Host $files[$j].name
+    }
+}
+
+# Create folder if it does not exist
+$folderpath="$PSScriptRoot\outfolder"
+if (Test-Path $folderpath){
+    Write-Host "Folder Already Exists"
+}
+else{
+    New-Item -ItemType Directory -Path $folderpath
+}
+
+# Save results to out.csv file in outfolder dir
+# Below is commented out to avoid error of out.csv already existing
+#cd $PSScriptRoot
+#$files = (Get-ChildItem)
+
+#$folderPath = "$PSScriptRoot/outfolder/"
+#$filePath = New-Item -ItemType File "$folderPath\out.csv"
+
+#$files | Where-Object { $_.Extension -eq ".ps1" } | Export-Csv -Path $filePath
+# Up to above warning is comment out
+
+# Without changing directory change all .csv files to .log and display all files
+$files = Get-ChildItem -Recurse -File
+$files | Rename-Item -NewName { $_.Name -replace '.csv','.log' }
+Get-ChildItem -Recurse -File
