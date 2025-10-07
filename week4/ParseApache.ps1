d1c6b37e0bfbe5558e932f11c331089442b6f877
Azraelx0 <cade.tmercer46@gmail.com>
2025-10-07 10:29:47 -0400

    Update ParseApache.ps1

--- week4/ParseApache.ps1
+++ week4/ParseApache.ps1
@@ -9,7 +9,7 @@ $words = $logsnotformatted[$i].Split(" ");
 $tableRecords += [PSCustomObject]@{ "IP" = $words[0]; `
                                     "Time" = $words[3].Trim('['); `
                                     "Method" = $words[5].Trim('"'); `
-                                    "Page" = $words[$6]; `
+                                    "Page" = $words[6]; `
                                     "Protocol" = $words[7]; `
                                     "Response" = $words[8]; `
                                     "Referrer" = $words[10]; `
