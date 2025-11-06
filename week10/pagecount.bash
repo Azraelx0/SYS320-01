e3d5e7deab1026d4056e6fdb2254b80b7469a6ea
cade <cade.tmercer46@gmail.com>
2025-11-05 22:35:05 -0500

    added countingCurlAccess function

--- week10/pagecount.bash
+++ week10/pagecount.bash
@@ -2,7 +2,14 @@
 
 function pageCount(){
 	file="/var/log/apache2/access.log"
-	pages=$(cat "$file" | cut -d' ' -f7 | sort -n | uniq -c)
+	local pages=$(cat "$file" | cut -d' ' -f7 | sort -n | uniq -c)
 	echo "$pages"	
 }
-pageCount
+#pageCount
+function countingCurlAccess(){
+	file="/var/log/apache2/access.log"
+        local pages=$(grep "curl" "$file" | cut -d' ' -f1,12 | sort -n | uniq -c)
+        echo "$pages"
+}
+countingCurlAccess
+
